acf5dfcce6f63d20f90adfea3000cd2c252b137e
hlasbeztvare-code <hlasbeztvare@gmail.com>
2026-03-26 11:18:36 +0100

    fix: overeni spravne slozky pro zapis

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-[#D4AF37] text-6xl font-black italic">TEST LUXURY STATUS: ACTIVE</h1>
+    </div>
+  );
+}
